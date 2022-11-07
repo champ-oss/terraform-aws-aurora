a08d1441266895b23351d3349679f43a2af734bf
Justin Reeves <reeves122@gmail.com>
2022-11-07 16:23:42 -0600

    create resources for aurora cluster

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,30 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_region" "current" {}
+
+module "vpc" {
+  source                   = "github.com/champ-oss/terraform-aws-vpc.git?ref=v1.0.39-9596bfc"
+  git                      = var.git
+  availability_zones_count = 2
+  retention_in_days        = 1
+}
+
+resource "aws_security_group" "test" {
+  name_prefix = "test-aurora-"
+  vpc_id      = module.vpc.vpc_id
+}
+
 module "this" {
-  source = "../../"
+  source                              = "../../"
+  cluster_identifier_prefix           = var.git
+  private_subnet_ids                  = module.vpc.private_subnets_ids
+  source_security_group_id            = aws_security_group.test.id
+  vpc_id                              = module.vpc.vpc_id
+  protect                             = false
+  skip_final_snapshot                 = true
+  backup_retention_period             = 1
+  iam_database_authentication_enabled = true
+  iam_auth_lambda_enabled             = true
 }
---
+++ variables.tf
@@ -0,0 +1,283 @@
+variable "availability_zones" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#availability_zones"
+  type        = list(string)
+  default     = null
+}
+
+variable "allow_major_version_upgrade" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#allow_major_version_upgrade"
+  default     = false
+  type        = bool
+}
+
+variable "backtrack_window" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#backtrack_window"
+  type        = number
+  default     = null
+}
+
+variable "backup_retention_period" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#backup_retention_period"
+  type        = number
+  default     = 35
+}
+
+variable "cidr_blocks" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule#cidr_blocks"
+  type        = list(string)
+  default     = ["10.0.0.0/8"]
+}
+
+variable "cluster_identifier_prefix" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#cluster_identifier_prefix"
+  type        = string
+  default     = "mysqldb-test"
+}
+
+variable "copy_tags_to_snapshot" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#copy_tags_to_snapshot"
+  type        = bool
+  default     = true
+}
+
+variable "database_name" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#database_name"
+  type        = string
+  default     = "this"
+}
+
+variable "db_cluster_instance_class" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#db_cluster_instance_class"
+  type        = string
+  default     = null
+}
+
+variable "db_cluster_parameter_group_name" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#db_cluster_parameter_group_name"
+  type        = string
+  default     = null
+}
+
+variable "db_instance_parameter_group_name" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#db_instance_parameter_group_name"
+  type        = string
+  default     = null
+}
+
+variable "db_snapshot_source_arn" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/db_snapshot#db_instance_identifier"
+  type        = string
+  default     = null
+}
+
+variable "enable_global_write_forwarding" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#enable_global_write_forwarding"
+  type        = string
+  default     = null
+}
+
+variable "enable_http_endpoint" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#enable_http_endpoint"
+  type        = bool
+  default     = false
+}
+
+variable "enabled_cloudwatch_logs_exports" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#enabled_cloudwatch_logs_exports"
+  type        = list(string)
+  default = [
+    "audit",
+    "error",
+    "general",
+    "slowquery"
+  ]
+}
+
+variable "engine" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#engine"
+  default     = "aurora-mysql"
+  type        = string
+}
+
+variable "engine_mode" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#engine_mode"
+  default     = "provisioned"
+  type        = string
+}
+
+variable "engine_version" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#engine_version"
+  type        = string
+  default     = "8.0.mysql_aurora.3.02.1"
+}
+
+variable "final_snapshot_identifier" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#final_snapshot_identifier"
+  type        = string
+  default     = null
+}
+
+variable "iam_database_authentication_enabled" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#iam_database_authentication_enabled"
+  default     = true
+  type        = bool
+}
+
+variable "iam_auth_docker_tag" {
+  description = "Docker tag of IAM Auth code to deploy"
+  type        = string
+  default     = "a5f43f9c4ab5d1b0be4923f58bff799e53d38753"
+}
+
+variable "iam_auth_lambda_enabled" {
+  description = "enable or disable the lambda for setting up iam auth"
+  default     = false
+  type        = bool
+}
+
+variable "iam_roles" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#iam_roles"
+  type        = list(string)
+  default     = null
+}
+
+variable "kms_key_id" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#kms_key_id"
+  type        = string
+  default     = null
+}
+
+variable "git" {
+  description = "Name of the Git repo"
+  type        = string
+  default     = "terraform-aws-aurora"
+}
+
+variable "global_cluster_identifier" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#global_cluster_identifier"
+  type        = string
+  default     = null
+}
+
+variable "iops" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#iops"
+  type        = string
+  default     = null
+}
+
+variable "master_username" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#master_username"
+  type        = string
+  default     = "root"
+}
+
+variable "min_capacity" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#min_capacity"
+  type        = number
+  default     = 0.5
+}
+
+variable "max_capacity" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#max_capacity"
+  type        = number
+  default     = 1
+}
+
+variable "mysql_iam_read_username" {
+  description = "read only user to be created via lambda function"
+  type        = string
+  default     = "db_iam_read"
+}
+
+variable "mysql_iam_admin_username" {
+  description = "admin user to be created via lambda function"
+  type        = string
+  default     = "db_iam_admin"
+}
+
+variable "network_type" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#network_type"
+  type        = string
+  default     = null
+}
+
+variable "port" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#port"
+  type        = string
+  default     = null
+}
+
+variable "preferred_backup_window" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#preferred_backup_window"
+  type        = string
+  default     = "06:00-06:30"
+}
+
+variable "preferred_maintenance_window" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#preferred_maintenance_window"
+  default     = "Sun:07:00-Sun:08:00"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_subnet_group#subnet_ids"
+  type        = list(string)
+}
+
+variable "protect" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#deletion_protection"
+  default     = true
+  type        = bool
+}
+
+variable "replication_source_identifier" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#replication_source_identifier"
+  type        = string
+  default     = null
+}
+
+variable "skip_final_snapshot" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#skip_final_snapshot"
+  default     = false
+  type        = bool
+}
+
+variable "snapshot_identifier" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#snapshot_identifier"
+  type        = string
+  default     = null
+}
+
+variable "source_region" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#source_region"
+  type        = string
+  default     = null
+}
+
+variable "source_security_group_id" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule#source_security_group_id"
+  type        = string
+}
+
+variable "storage_type" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#storage_type"
+  type        = string
+  default     = null
+}
+
+variable "storage_encrypted" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#storage_encrypted"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Map of tags to assign to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group#vpc_id"
+  type        = string
+}
