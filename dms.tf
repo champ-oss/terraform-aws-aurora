109c7422dbfc9c5a7cc0ef94a39978502946af69
Justin Reeves <reeves122@gmail.com>
2022-11-11 12:00:08 -0600

    change engine_name

--- dms.tf
+++ dms.tf
@@ -2,7 +2,7 @@ resource "aws_dms_endpoint" "this" {
   count         = var.create_dms_endpoint ? 1 : 0
   endpoint_id   = "${aws_rds_cluster.this.cluster_identifier}-dms-endpoint"
   endpoint_type = var.dms_endpoint_type
-  engine_name   = var.engine
+  engine_name   = "aurora"
   database_name = var.database_name
   password      = random_password.password.result
   port          = aws_rds_cluster.this.port
