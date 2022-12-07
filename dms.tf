9a02eff0b874c8b7bb37f70719f7ac16e6558974
Justin Reeves <reeves122@gmail.com>
2022-12-07 10:05:28 -0600

    remove redundant HistoryTimeslotInMinutes

--- dms.tf
+++ dms.tf
@@ -187,7 +187,6 @@ resource "aws_dms_replication_task" "this" {
       "BeforeImageSettings" : null,
       "ControlTablesSettings" : {
         "historyTimeslotInMinutes" : 5,
-        "HistoryTimeslotInMinutes" : 5,
         "StatusTableEnabled" : false,
         "SuspendedTablesTableEnabled" : false,
         "HistoryTableEnabled" : false,
