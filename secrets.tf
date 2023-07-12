ed392a173e4788f507f9b21d6f2bd6b012e37230
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-07-12 17:03:23 -0500

    resolve linting issue

--- secrets.tf
+++ secrets.tf
@@ -1,10 +1,10 @@
 resource "aws_secretsmanager_secret_version" "this" {
-  count = var.enable_glue_connection ? 1 : 0
+  count         = var.enable_glue_connection ? 1 : 0
   secret_id     = aws_secretsmanager_secret.this.id
   secret_string = random_password.password.result
 }
 
 resource "aws_secretsmanager_secret" "this" {
   count = var.enable_glue_connection ? 1 : 0
-  name = "${var.name}-${random_string.identifier.result}"
+  name  = "${var.name}-${random_string.identifier.result}"
 }
