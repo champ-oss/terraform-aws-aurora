f736adcefe7050ca9afa773bae5e6de9af089418
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-07-13 11:09:29 -0500

    adding sg for glue

---
+++ security_group.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "rds" {
+  name_prefix = "${var.cluster_identifier_prefix}-rds-"
+  vpc_id      = var.vpc_id
+  tags        = merge(local.tags, var.tags)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "from_sg" {
+  description              = "ingress from security group"
+  type                     = "ingress"
+  from_port                = aws_rds_cluster.this.port
+  to_port                  = aws_rds_cluster.this.port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds.id
+  source_security_group_id = var.source_security_group_id
+}
+
+resource "aws_security_group_rule" "from_cidr" {
+  description       = "ingress from cidr blocks"
+  type              = "ingress"
+  from_port         = aws_rds_cluster.this.port
+  to_port           = aws_rds_cluster.this.port
+  protocol          = "tcp"
+  security_group_id = aws_security_group.rds.id
+  cidr_blocks       = var.cidr_blocks
+}
+
+resource "aws_security_group" "glue" {
+  count       = var.enable_glue_connection ? 1 : 0
+  name_prefix = "${var.cluster_identifier_prefix}-glue-"
+  vpc_id      = var.vpc_id
+  tags        = merge(local.tags, var.tags)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "glue" {
+  count                    = var.enable_glue_connection ? 1 : 0
+  description              = "ingress from security group"
+  type                     = "ingress"
+  from_port                = aws_rds_cluster.this.port
+  to_port                  = aws_rds_cluster.this.port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds.id
+  source_security_group_id = var.source_security_group_id
+}
