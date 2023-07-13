2ed382761ff317099c68bdd924f83e44e0ce592b
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-07-13 13:23:07 -0500

    remove random string, not used

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-resource "random_string" "identifier" {
-  length  = 5
-  special = false
-  upper   = false
-  lower   = true
-  number  = true
-}
-
 locals {
   snapshot_timestamp = formatdate("'${var.cluster_identifier_prefix}-'YYYYMMDDHHmmss", timestamp())
   db_snapshot_source = var.db_snapshot_source_arn != null ? data.aws_db_snapshot.this[0].id : null
