073e317ae5be24fc8fd3c9b9fd3dc162dbac9757
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-07-13 11:09:16 -0500

    adding sg for glue

--- glue_connection.tf
+++ glue_connection.tf
@@ -9,7 +9,7 @@ resource "aws_glue_connection" "this" {
   }
 
   physical_connection_requirements {
-    security_group_id_list = aws_security_group.this[0].id
+    security_group_id_list = aws_security_group.glue[0].id
     subnet_id              = element(aws_db_subnet_group.this.subnet_ids, 0)
   }
 }
