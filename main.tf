8f2e08f83e447c51ed3c85e40317727ec750da2b
Justin Reeves <reeves122@gmail.com>
2023-09-15 16:29:16 -0500

    use trimsuffix to remove any trailing hyphens

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ locals {
   snapshot_timestamp = formatdate("'${var.cluster_identifier_prefix}-'YYYYMMDDHHmmss", timestamp())
   db_snapshot_source = var.db_snapshot_source_arn != null ? data.aws_db_snapshot.this[0].id : null
 
+  # 60 character max length with 27 character random suffix
+  cluster_identifier_prefix = trimsuffix(substr(var.cluster_identifier_prefix, 0, 33), "-")
+
   tags = {
     cost    = "aurora"
     creator = "terraform"
