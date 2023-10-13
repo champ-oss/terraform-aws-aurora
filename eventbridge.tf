40260d44eded85275eb7482b3408feb388b8bdff
Justin Reeves <reeves122@gmail.com>
2023-10-13 15:56:50 -0500

    add hyphen to eventbridge naming

--- eventbridge.tf
+++ eventbridge.tf
@@ -1,12 +1,12 @@
 module "eventbridge_s3_export" {
   count        = var.enable_s3_export ? 1 : 0
   source       = "github.com/champ-oss/terraform-aws-eventbridge.git?ref=v1.0.2-b5d961e"
-  git          = "${substr(local.cluster_identifier_prefix, 0, 28)}s3-export"
+  git          = "${substr(local.cluster_identifier_prefix, 0, 28)}-s3-export"
   service_name = "rds"
   api_action   = "startExportTask"
   role_arn     = aws_iam_role.this[0].arn
   input_parameters = {
-    ExportTaskIdentifier : "${local.cluster_identifier_prefix}<aws.scheduler.execution-id>"
+    ExportTaskIdentifier : "${local.cluster_identifier_prefix}-<aws.scheduler.execution-id>"
     IamRoleArn : aws_iam_role.this[0].arn,
     KmsKeyId : module.kms[0].key_id,
     S3BucketName : module.s3[0].bucket,
