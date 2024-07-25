a668d4f2d1a772ee4fe227ccb080ddf5744361db
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-25 16:28:48 -0500

    testing with sg set to null

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -25,11 +25,12 @@ data "aws_subnets" "this" {
     values = [data.aws_vpcs.this.ids[0]]
   }
 }
-
+/*
 resource "aws_security_group" "test" {
   name_prefix = "test-aurora-"
   vpc_id      = data.aws_vpcs.this.ids[0]
 }
+*/
 
 module "this" {
   source                              = "../../"
@@ -40,7 +41,7 @@ module "this" {
   private_subnet_ids                  = data.aws_subnets.this.ids
   protect                             = false
   skip_final_snapshot                 = true
-  source_security_group_id            = aws_security_group.test.id
+  # source_security_group_id            = aws_security_group.test.id
   vpc_id                              = data.aws_vpcs.this.ids[0]
   publicly_accessible                 = true
   enable_glue_connection              = true
