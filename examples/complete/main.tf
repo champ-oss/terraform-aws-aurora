bd37819155810f0fadff991c682e5797c4c0d663
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-25 22:23:37 -0500

    testing

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -41,7 +41,7 @@ module "this" {
   private_subnet_ids                  = data.aws_subnets.this.ids
   protect                             = false
   skip_final_snapshot                 = true
-  enable_source_security_group        = true
+  enable_source_security_group        = false
   source_security_group_id            = aws_security_group.test.id
   vpc_id                              = data.aws_vpcs.this.ids[0]
   publicly_accessible                 = true
