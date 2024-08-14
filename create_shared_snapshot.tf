b8302e93592a6e5ded597b3e074134fe2bab6004
Phillip Langley <philliplangley@champmbplangley.lan>
2024-08-14 08:58:43 -0500

    removing key value tag as not needed

--- create_shared_snapshot.tf
+++ create_shared_snapshot.tf
@@ -3,5 +3,5 @@ resource "aws_db_cluster_snapshot" "this" {
   db_cluster_identifier          = aws_rds_cluster.this[0].id
   db_cluster_snapshot_identifier = var.db_cluster_snapshot_identifier != null ? var.db_cluster_snapshot_identifier : substr("${aws_rds_cluster.this[0].id}-shared", 0, 63)
   shared_accounts                = var.shared_accounts_snapshot
-  tags                           = merge(local.tags, var.tags, { status = "shared-snapshot" })
+  tags                           = merge(local.tags, var.tags)
 }
