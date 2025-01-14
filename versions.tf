d0b948d1c26de72dbfa33dd1cebccf274f8c4086
Phillip Langley <philliplangley@champmbplangley.lan>
2025-01-14 10:53:42 -0600

    testing auto pause feature

--- versions.tf
+++ versions.tf
@@ -1,9 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.68.0"
-    }
+  aws = {
+    source  = "hashicorp/aws"
+    version = ">= 5.40.0, <= 5.68.0"
   }
 }
