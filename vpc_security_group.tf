f61e3026c8652f69a7ff7f4d74ddd9538b84be81
Phillip Langley <philliplangley@champmbplangley.lan>
2025-04-03 15:39:22 -0500

    test removing old security groups

---
+++ vpc_security_group.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc_security_group_ingress_rule" "from_sg" {
+  count                        = var.enabled && var.enable_source_security_group && !var.enabled_legacy_secruity_group_rule ? 1 : 0
+  description                  = "ingress from security group"
+  from_port                    = aws_rds_cluster.this[0].port
+  to_port                      = aws_rds_cluster.this[0].port
+  ip_protocol                  = "tcp"
+  security_group_id            = aws_security_group.rds[0].id
+  referenced_security_group_id = var.source_security_group_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "from_cidr" {
+  count             = var.enabled && !var.enabled_legacy_secruity_group_rule ? 1 : 0
+  description       = "ingress from cidr blocks"
+  from_port         = aws_rds_cluster.this[0].port
+  to_port           = aws_rds_cluster.this[0].port
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.rds[0].id
+  cidr_ipv4         = var.cidr_blocks
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_self" {
+  count                        = var.enabled && !var.enabled_legacy_secruity_group_rule ? 1 : 0
+  description                  = "ingress self reference"
+  from_port                    = 0
+  to_port                      = 65535
+  ip_protocol                  = "tcp"
+  security_group_id            = aws_security_group.rds[0].id
+  referenced_security_group_id = aws_security_group.rds[0].id
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress" {
+  count             = var.enabled && !var.enabled_legacy_secruity_group_rule ? 1 : 0
+  description       = "egress internet"
+  from_port         = 0
+  to_port           = 65535
+  ip_protocol       = "all"
+  security_group_id = aws_security_group.rds[0].id
+  cidr_ipv4         = ["0.0.0.0/0"]
+
+}
